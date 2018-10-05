2b792c5e3b05630cc68ecb82de7b3c8e7900746e
August H. Hummert V <august.hummert5@gmail.com>
2018-10-05 13:26:50 -0500

    too much JTable, now Exceptions

---
+++ src/Exceptions/CopyFile.java
@@ -0,0 +1,21 @@
+package Exceptions;
+import java.io.FileNotFoundException;
+import java.util.*;
+public class CopyFile {
+
+
+
+    public static void main(String [] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        String fileName;
+
+        System.out.print("Enter the name of a file: ");
+        fileName = scan.next();
+
+        Scanner fileScan = new Scanner(fileName);
+
+    }
+
+    }
+}
