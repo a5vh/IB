346546a99998f475b4d1381ce2a40a977b094dff
August H. Hummert V <august.hummert5@gmail.com>
2018-10-15 13:24:04 -0500

    reading/writing to CSV

--- src/Exceptions/CSVProgram.java
+++ src/Exceptions/CSVProgram.java
@@ -4,28 +4,67 @@ package Exceptions;
 import java.io.File;
 import java.io.FileReader;
 import java.util.Scanner;
+import java.io.*;
 
 public class CSVProgram {
 
     public static void main (String [] args)
     {
-        try{
-            File file = new File("src/SISData.csv");
-            FileReader fr = new FileReader(file);
-            CSVReader csv = new CSVReader(fr);
+        File file = new File("src/Students.csv");
+        BufferedReader br = null;
+        Scanner scan = new Scanner(System.in);
 
-            String[] nextRecord;
+        String nextLine = " ";
+        String cvsSplitBy = ",";
+        String cont = "y";
+        String name = " ";
+        String gradeinClass = " ";
+        String grade = " ";
+        String assignments = " ";
 
-            while((nextRecord = csv.readNext()) != null) {
-                for (String cell : nextRecord) {
-                    System.out.print(cell + "\t");
-                }
+        try {
 
-                System.out.println();
+            br = new BufferedReader(new FileReader(file));
+            while ((nextLine = br.readLine()) != null) {
+
+                String[] Students = nextLine.split(cvsSplitBy);
+
+                System.out.println("Name: " + Students[0] + "\n" + "Grade in Class: " + Students[1]
+                        + "%" + "\n" + "Grade: " + Students[2] + "\n" + "All assignments turned in?: "
+                        + Students[3] + "\n");
             }
-        } catch (Exception e)
+            br.close();
+
+            } catch (Exception e)
         {
             e.printStackTrace();
+        }
+
+        while (cont.equalsIgnoreCase("y") || cont.equalsIgnoreCase("yes"))
+        {
+            System.out.println("Enter the name of the student.");
+            name = scan.next();
+            System.out.println("Enter " + name + "'s grade in the current class.");
+            gradeinClass = scan.next();
+            System.out.println("Enter " + name + "'s grade.");
+            grade = scan.next();
+            System.out.println("Does " + name + " have all their assignmnets done?");
+            assignments = scan.next();
+
+            try {
+                FileWriter fr = new FileWriter(file, true);
+                BufferedWriter bw = new BufferedWriter(fr);
+                PrintWriter out = new PrintWriter(bw);
+
+                out.println("\n" + name + "," + gradeinClass + "," + grade + "," + assignments);
+                out.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Any more objects?");
+            cont = scan.next();
         }
     }
 }
