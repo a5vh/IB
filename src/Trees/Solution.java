b3c34d2098697905c7b583fea5b53c7112d5b141
August H. Hummert V <august.hummert5@gmail.com>
2018-11-26 13:23:03 -0600

    intro to Trees

---
+++ src/Trees/Solution.java
@@ -0,0 +1,26 @@
+package Trees;
+
+import java.io.*;
+
+
+public class Solution {
+
+    public static void bubblesort(int[] array)
+    {
+
+    }
+
+
+    public static void swap(int[] array, int i, int j)
+    {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
+
+    public static void main(String [] args)
+    {
+
+    }
+}
+
