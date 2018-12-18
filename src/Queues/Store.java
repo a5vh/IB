445e99819e92a421f38db88acd785e24fa58f321
August H. Hummert V <august.hummert5@gmail.com>
2018-12-18 13:30:40 -0600

    redo of Store

--- src/Queues/Store.java
+++ src/Queues/Store.java
@@ -1,116 +1,37 @@
 package Queues;
 
-import javafx.scene.control.Tab;
-
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.JTableHeader;
-import javax.swing.table.TableColumn;
-import javax.swing.table.TableColumnModel;
-import java.awt.*;
-import java.awt.event.WindowEvent;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.util.*;
-import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class Store {
 
-    public static void main (String [] args)
+    JFrame cart;
+    JFrame checkout;
+    JFrame bags;
+
+    JList
+
+    public Store()
     {
-        JFrame shoppingCartList = new JFrame("Shopping Cart");
-        DefaultTableModel model = new DefaultTableModel(0, 3);
-        JTable table = new JTable(model);
+        cart = new JFrame("Cart");
+        checkout = new JFrame("Checkout Line");
+        bags = new JFrame("Bags");
+    }
 
-        JTableHeader th = table.getTableHeader();
-        TableColumnModel tcm = th.getColumnModel();
+    public static void main (String[] args)
+    {
+        Stack<Item> itemStack = new Stack();
+        Queue<Item> itemQueue = new LinkedList<Item>();
 
-        TableColumn tc0 = tcm.getColumn(0);
-        TableColumn tc1 = tcm.getColumn(1);
-        TableColumn tc2 = tcm.getColumn(2);
+        ArrayList<String> bigBags = new ArrayList<>();
+        ArrayList<String> medBags = new ArrayList<>();
+        ArrayList<String> smaBags = new ArrayList<>();
 
-        tc0.setHeaderValue("Name of Item");
-        tc1.setHeaderValue("Price");
-        tc2.setHeaderValue("Size");
-
-        Queue<Item> checkoutLine = new LinkedList<>();
-        Stack shoppingCart = new Stack();
-
-        double total = 0.00;
-
-        ShoppingCart cart = new ShoppingCart(shoppingCart);
-        CheckoutLine checkout = new CheckoutLine(checkoutLine, shoppingCart);
-        NumberFormat formatter = new DecimalFormat("$#0.00");
-
-        System.out.println(shoppingCart.size());
-
-        cart.addToCart("Peaches", 2, "small");
-        cart.addToCart("Pears", 3, "small");
-        cart.addToCart("Dogfood", 21, "large");
-        cart.addToCart("Milk", 6, "medium");
-
-        Object[] jlist = new Object[shoppingCart.size()];
-        for (int i = 0; i < shoppingCart.size(); i++)
-        {
-            jlist[i] = shoppingCart.get(i);
-        }
-
-        JList list = new JList(jlist);
-        list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
-        list.setLayoutOrientation(JList.VERTICAL);
-        list.setVisibleRowCount(-1);
-
-
-        checkout.stackToQueue(shoppingCart);
-
-        JOptionPane.showMessageDialog(shoppingCartList, "Items transferred to conveyor belt \nfor scanning and bagging.");
-
-        ((LinkedList<Item>) checkoutLine).sort(Comparator.comparing(Item::getSize));
-
-
-
-        for (int i = 0; i < checkoutLine.size(); i++)
-        {
-            Item item = checkoutLine.poll();
-            double currentPrice = item.getPrice();
-            total += currentPrice;
-            checkoutLine.add(item);
-        }
-
-        for (int i = 0; i < checkoutLine.size(); i++)
-        {
-            Item item = checkoutLine.poll();
-            System.out.println(item.getItemName());
-            checkoutLine.add(item);
-        }
-
-        System.out.println("\n");
-
-        checkout.printCheckout();
-
-        checkout.bagItems();
-
-        System.out.println("\n" + formatter.format(total));
-
-        shoppingCartList.add(new JScrollPane(table));
-
-        JScrollPane listScroller = new JScrollPane(list);
-        listScroller.setPreferredSize(new Dimension(250, 80));
-        shoppingCartList.add(new JScrollPane(list));
-
-        shoppingCartList.setSize(400,500);
-        shoppingCartList.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        shoppingCartList.setVisible(true);
-
-
-
-        //String name = JOptionPane.showInputDialog(frame, "What's the name of the item?");
-        //String price = JOptionPane.showInputDialog(frame, "What's the price of " + name + "?");
-        //String size = JOptionPane.showInputDialog(frame, "What is the size of " + name + "?");
-
+        Store store = new Store();
 
 
     }
-
-
 }
--- src/Trees/IntList.java
+++ src/Trees/IntList.java
@@ -8,16 +8,19 @@ import java.awt.*;
 public class IntList {
 
     JFrame frame;
+    DefaultListModel dm;
 
     IntList()
     {
         frame = new JFrame("Int List");
+        dm = new DefaultListModel();
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.setSize(300, 300);
         frame.setVisible(true);
     }
+
 
     public static void main (String[] args) {
 
@@ -27,40 +30,34 @@ public class IntList {
 
         IntList intList = new IntList();
 
-        String stringTimes = JOptionPane.showInputDialog("How many integers would you like\n to enter?");
-        int intTimes = Integer.parseInt(stringTimes);
 
+        int intTimes = Integer.parseInt(JOptionPane.showInputDialog("How many integers would you like\n to enter?"));
 
         for (int i = 0; i < intTimes; i++) {
             String integer = JOptionPane.showInputDialog("What is your integer?");
             head.insert(integer);
         }
 
+
         head.printInOrder(arrayList);
         System.out.println(head.contains("5"));
 
-        DefaultListModel dm = new DefaultListModel();
+
         for (String i : arrayList)
         {
-            dm.addElement(i);
+            intList.dm.addElement(i);
         }
 
         JList list = new JList();
         JPanel panel = new JPanel();
-        JButton add = new JButton("Add");
-        add.setSize(5, 5);
 
-        Dimension d = new Dimension(10, 10);
-        add.setPreferredSize(d);
-
-        GridLayout gridLayout = new GridLayout(2, 1);
+        GridLayout gridLayout = new GridLayout(1, 1);
 
         panel.setLayout(gridLayout);
 
-        list.setModel(dm);
+        list.setModel(intList.dm);
 
         panel.add(list);
-        panel.add(add);
 
         intList.frame.add(panel);
 
